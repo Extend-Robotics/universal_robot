25581c30a7d90e3fa85e9310b3e7da76228f4510
Nitish <nitish.sharma@extendrobotics.com>
2023-06-19 11:30:24 +0100

    Added Gripper Control and UR10e Joints and Kinematics change

---
+++ extend_gripper_control/scripts/gripper_control_digital.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rosnode
+import rospy
+import sys
+import copy
+import rospkg
+import extend_msgs
+from extend_msgs.msg import GripperControl
+from std_msgs.msg import String
+import ur_msgs.srv
+
+#Creating the ros node and service client
+rospy.init_node("ur_gripper")
+rospy.wait_for_service("/ur_hardware_interface/set_io")
+
+def dataCallback(msg):
+    if(msg.gripperDigital.data):
+        gripperValue = 1
+    else:
+        gripperValue = 0
+    gripperPin = msg.gripperPin.data
+    gripperControl = rospy.ServiceProxy("/ur_hardware_interface/set_io", ur_msgs.srv.SetIO)
+    gripperAction = gripperControl(1,gripperPin,gripperValue)
+    
+if __name__ == '__main__': 
+    #Subscribe to Digital Gripper Data Stream from Unity   
+    rospy.Subscriber("extend_gripper_command", GripperControl, dataCallback)
+    rospy.spin() 
